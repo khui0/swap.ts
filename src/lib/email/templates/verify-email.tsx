53866edd4e52b2a8a9c52e422788e868336bf7d4
Kenny <101839505+khui0@users.noreply.github.com>
2025-11-06 14:42:47 -0500

    feat: new verify email template

--- src/lib/email/templates/verify-email.tsx
+++ src/lib/email/templates/verify-email.tsx
@@ -1,11 +1,43 @@
-import { Html } from "@react-email/html";
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
 
-export default function VerifyEmail({ url }: { url: string }) {
+export default function VerifyEmail({ name, url }: { name: string; url: string }) {
   return (
     <Html>
-      <h1>Verify your email</h1>
-      <p>Click below:</p>
-      <a href={url}>Verify Email</a>
+      <Head>
+        <meta name="color-scheme" content="light" />
+        <meta name="supported-color-schemes" content="light" />
+      </Head>
+      <Tailwind config={{}}>
+        <Body className="bg-[#f5f5f5] p-4">
+          <Preview>Verify your email address</Preview>
+          <Container className="rounded-xl bg-white px-4">
+            <Section>
+              <Text>Hi {name},</Text>
+              <Text>Thank you for signing up. Please verify your email address below:</Text>
+              <Button
+                className="inline-flex h-10 items-center justify-center rounded-lg bg-black px-4 font-semibold text-white"
+                href={url}
+              >
+                Verify Email
+              </Button>
+              <Text>
+                If the button above didn't work, copy and paste this link into your browser:
+              </Text>
+              <a href={url}></a>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 }
